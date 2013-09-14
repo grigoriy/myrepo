5f852e5b0d9609ee911da9bb14a0737da0fba1e0
Grigoriy Alexeev <grigoriyalexeev@gmail.com>
2013-09-14 19:08:00 -0400

    Added GetInput.java and test_cloud9.py

---
+++ GetInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class GetInput {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Print smth: ");
+        String inp = scan.nextLine();
+        System.out.println(inp);
+    }
+}
---
+++ test_cloud9.py
@@ -0,0 +1,3 @@
+!#/usr/bin/env python
+
+print("This file was added to cloud9 master branch of 'myrepo'")
